0181d4eaf188aa13693b51296ca3654dca8a59a1
Jerry George <jerrygb@gmail.com>
2021-01-31 18:38:19 -0500

    Update to v1.2.0

--- Formula/kfctl.rb
+++ Formula/kfctl.rb
@@ -1,9 +1,9 @@
 class Kfctl < Formula
   desc "Kubeflow CLI tool"
   homepage "https://www.kubeflow.org"
-  url "https://github.com/kubeflow/kubeflow/releases/download/v0.6.2/kfctl_v0.6.2_darwin.tar.gz"
-  version "0.6.2"
-  sha256 "3e409fad6cf040cd1fbc1ffeb0c47862528e1595e0dde70730d994b0a4b7e3dd"
+  url "https://github.com/kubeflow/kfctl/releases/download/v1.2.0/kfctl_v1.2.0-0-gbc038f9_darwin.tar.gz"
+  version "1.2.0"
+  sha256 "c542d8e5872db4181cefa7feb83058fdf34174a6a7d1bb33600da855f3cf0096"
   head "https://github.com/kubeflow/kubeflow.git"
 
   bottle :unneeded
